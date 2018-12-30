906a1b73a4f4c9abf99ef5a6a9d74460dea7dfa7
JesusPartal <duncan.rsr@gmail.com>
2018-12-30 21:18:12 +0000

    Select Destination WIP

--- app/src/main/java/cit/jauc/PickDestinationActivity.java
+++ app/src/main/java/cit/jauc/PickDestinationActivity.java
@@ -2,32 +2,42 @@ package cit.jauc;
 
 import android.content.Context;
 import android.graphics.PointF;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.api.geocoding.v5.GeocodingCriteria;
+import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
+import com.mapbox.mapboxsdk.location.LocationServices;
+import com.mapbox.core.exceptions.ServicesException;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerView;
 import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.style.light.Position;
-
-
 import cit.jauc.model.PulseMarkerView;
-
-
+import cit.jauc.model.PulseMarkerViewOptions;
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//import retrofit2.Response;
 
 public class PickDestinationActivity extends AppCompatActivity {
 
@@ -39,8 +49,8 @@ public class PickDestinationActivity extends AppCompatActivity {
 
     private ImageView dropPinView;
     private Marker addressPin;
-    private ImageButton clearDisplayViewButton;
     private MarkerView userMarker;
+    private Button clearDisplayViewButton;
 
 
     @Override
@@ -48,63 +58,30 @@ public class PickDestinationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_destination);
 
-        Mapbox.getInstance(getApplicationContext(), getString(R.string.mapbox_access_token));
-
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
-        ImageView dropPinView = new ImageView(this);
-        dropPinView.setImageResource(R.drawable.ic_droppin_24dp);
+        createDropPin();
+        createSelectLocationButton();
+        createClearSelectionButton();
 
-
-
-
-        mapView.getMapAsync(new OnMapReadyCallback() {
+         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap map) {
 
                 mapboxMap = map;
 
+                //Add user marker
+                mapboxMap.getMarkerViewManager().addMarkerViewAdapter(new PulseMarkerViewAdapter(PickDestinationActivity.this));
+                userMarker = createCustomUserMarker(new LatLng(0, 0));
 
-                // User Marker
-//                mapboxMap.getMarkerViewManager().addMarkerViewAdapter(new PulseMarkerViewAdapter(PickDestinationActivity.this));
+                //
+                PointF focalPoint = new PointF((mapView.getX() + mapView.getWidth() / 2), (mapView.getY() + mapView.getHeight() / 2));
+                mapboxMap.getUiSettings().setFocalPoint(focalPoint);
 
-                // Create drop pin using custom image
-
-
-                // drop pin in center of screen
-                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
-                float density = getResources().getDisplayMetrics().density;
-                params.bottomMargin = (int) (12 * density);
-                dropPinView.setLayoutParams(params);
-                mapView.addView(dropPinView);
-
-                // Make drop pin invisible
-//                dropPinView.setVisibility(View.INVISIBLE);
-
-                // Get LatLng of selected location
-                LatLng position = mapboxMap.getProjection().fromScreenLocation(new PointF(dropPinView.getLeft() + (dropPinView.getWidth() / 2), dropPinView.getBottom()));
-
-                // Remove previous address pin (if exists)
-                if (addressPin != null) {
-                    if (mapboxMap != null && addressPin != null) {
-                        mapboxMap.removeMarker(addressPin);
-                    }
-                }
-
-                addressPin = mapboxMap.addMarker(new MarkerViewOptions().title("Loading address…").position(position));
-                mapboxMap.selectMarker(addressPin);
-
-                //Create Geocoding client
-                MapboxGeocoding client = new MapboxGeocoding.Builder()
-                        .setAccessToken(getString(R.string.mapbox_access_token))
-                        .setCoordinates(Position.fromCoordinates(position.getLongitude(), position.getLatitude()))
-                        .setType(GeocodingCriteria.TYPE_ADDRESS)
-                        .build();
-
+                trackUserLocationView(userMarker);
             }
         });
-
     }
 
 
@@ -150,6 +127,261 @@ public class PickDestinationActivity extends AppCompatActivity {
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
+    }
+
+    private void zoomInOn(Location location) {
+        //Move the camera to the user
+        if (location != null) {
+            mapboxMap.setCameraPosition(new CameraPosition.Builder()
+                    .target(new LatLng(location))
+                    .zoom(16)
+                    .bearing(0)
+                    .tilt(0)
+                    .build());
+        }
+    }
+
+
+    /**
+     * Tracks the camera to animate the marker when overlapping with the picker.
+     * Makes sure the marker actually points to the user's position by tracking it.
+     */
+    private void trackUserLocationView(final MarkerView markerView) {
+        final float circleDiameterSize = getResources().getDimension(R.dimen.circle_size);
+
+        //Track camera changes to check for overlap
+        mapboxMap.setOnCameraChangeListener(new MapboxMap.OnCameraChangeListener() {
+
+            private Animation pulseAnimation;
+
+            @Override
+            public void onCameraChange(CameraPosition position) {
+                if (markerView == null) {
+                    return;
+                }
+
+                //Make drop pin visible, if it wasn't already
+                showDropPin();
+
+                //Get the distance from the tip of the location picker to the MarkerView
+                double distance = getLocationPickerLocation().distanceTo(markerView.getPosition());
+
+                //If closeby, animate, otherwise, stop animation
+                View view = mapboxMap.getMarkerViewManager().getView(markerView);
+                if (view != null) {
+                    View backgroundView = view.findViewById(R.id.background_imageview);
+                    if (pulseAnimation == null && distance < 0.5f * circleDiameterSize) {
+                        pulseAnimation = AnimationUtils.loadAnimation(PickDestinationActivity.this, R.anim.pulse);
+                        pulseAnimation.setRepeatCount(Animation.INFINITE);
+                        pulseAnimation.setRepeatMode(Animation.RESTART);
+                        backgroundView.startAnimation(pulseAnimation);
+                    } else if (pulseAnimation != null && distance >= 0.5f * circleDiameterSize) {
+                        backgroundView.clearAnimation();
+                        pulseAnimation = null;
+                    }
+                }
+            }
+        });
+
+        //Track location updates to move the user marker
+        LocationServices.getLocationServices(getApplicationContext()).addLocationListener(new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (location != null && markerView != null) {
+                    markerView.setPosition(new LatLng(location));
+                }
+            }
+        });
+
+    }
+
+    private void createDropPin() {
+        float density = getResources().getDisplayMetrics().density;
+
+        dropPinView = new ImageView(this);
+        dropPinView.setImageResource(R.drawable.ic_droppin_24dp);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+        params.bottomMargin = (int) (12 * density);
+        dropPinView.setLayoutParams(params);
+
+        mapView.addView(dropPinView);
+    }
+
+    private void showDropPin() {
+        if (dropPinView != null && dropPinView.getVisibility() != View.VISIBLE) {
+            dropPinView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void hide(View view) {
+        if (view != null) {
+            view.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void createClearSelectionButton() {
+        clearDisplayViewButton = (Button) findViewById(R.id.bt_clearDisplayView);
+        clearDisplayViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeAddressPin();
+                hide(clearDisplayViewButton);
+                showDropPin();
+            }
+        });
+    }
+
+    private MarkerView createCustomUserMarker(LatLng markerPosition) {
+        return mapboxMap.addMarker(new PulseMarkerViewOptions()
+                .icon(IconFactory.getInstance(getApplicationContext()).fromResource(R.drawable.ic_my_location_24dp))
+                .position(markerPosition)
+        );
+    }
+
+    private void createSelectLocationButton() {
+        Button selectLocationButton = (Button) findViewById(R.id.bt_selectLocation);
+        //noinspection ConstantConditions
+        selectLocationButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i(TAG, "Location Selected!");
+                        if (mapboxMap != null) {
+                            //Control button's state
+                            clearDisplayViewButton.setVisibility(View.VISIBLE);
+                            dropPinView.setVisibility(View.INVISIBLE);
+
+                            //Get position for the drop pin
+                            LatLng position = getLocationPickerLocation();
+
+                            //Show the address pin (result)
+                            showAddressPin(position);
+
+                            //Get the address for that location and update the marker
+                            geocode(position, new GeocodeCallbacks() {
+                                @Override
+                                public void onResult(String result) {
+                                    updateAddressPin(result);
+                                }
+
+                                @Override
+                                public void onFailure(Throwable failure) {
+                                    showFeedbackMessage("Could not retrieve address: " + failure.getMessage());
+                                }
+                            });
+                        }
+                    }
+                }
+        );
+    }
+
+
+    private void geocode(LatLng point, final GeocodeCallbacks callbacks) {
+        try {
+            //Create Geocoding client
+            MapboxGeocoding client = new MapboxGeocoding.Builder()
+                    .setAccessToken(getString(R.string.mapbox_access_token))
+                    .setCoordinates(Position.fromCoordinates(point.getLongitude(), point.getLatitude()))
+                    .setType(GeocodingCriteria.TYPE_ADDRESS)
+                    .build();
+
+            //Place the request
+            client.enqueueCall(new Callback<GeocodingResponse>() {
+                @Override
+                public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+
+                    List<GeocodingFeature> results = response.body().getFeatures();
+                    String address = null;
+                    if (results.size() > 0) {
+                        GeocodingFeature feature = results.get(0);
+                        address = feature.getAddress() + " " + feature.getText();
+                        Log.i(TAG, "address " + address);
+                    } else {
+                        showFeedbackMessage("No results for search.");
+                    }
+
+                    callbacks.onResult(address);
+                }
+
+                @Override
+                public void onFailure(Call<GeocodingResponse> call, Throwable t) {
+                    Log.e(TAG, "Geocoding Failure: " + t.getMessage());
+                    callbacks.onFailure(t);
+                }
+            });
+        } catch (ServicesException e) {
+            Log.e(TAG, "Error geocoding: " + e.toString());
+            callbacks.onFailure(e);
+        }
+
+    }
+
+    private LatLng getLocationPickerLocation() {
+        return mapboxMap.getProjection().fromScreenLocation(
+                new PointF(dropPinView.getLeft() + (dropPinView.getWidth() / 2), dropPinView.getBottom())
+        );
+    }
+
+    private Marker showAddressPin(LatLng position) {
+        if (addressPin != null) {
+            //Remove previous pin
+            removeAddressPin();
+        }
+
+        //Create new one
+        addressPin = mapboxMap.addMarker(new MarkerViewOptions().title("Loading address...").position(position));
+        mapboxMap.selectMarker(addressPin);
+        return addressPin;
+    }
+
+    private void removeAddressPin() {
+        if (mapboxMap != null && addressPin != null) {
+            mapboxMap.removeMarker(addressPin);
+        }
+    }
+
+    private void updateAddressPin(@Nullable String address) {
+        if (addressPin != null) {
+            addressPin.setTitle(address == null ? "No address found" : address);
+        }
+    }
+
+    private void showFeedbackMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private static class PulseMarkerViewAdapter extends MapboxMap.MarkerViewAdapter<PulseMarkerView> {
+
+        private LayoutInflater inflater;
+
+        public PulseMarkerViewAdapter(@NonNull Context context) {
+            super(context);
+            this.inflater = LayoutInflater.from(context);
+        }
+
+        @Nullable
+        @Override
+        public View getView(@NonNull PulseMarkerView marker, @Nullable View convertView, @NonNull ViewGroup parent) {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                convertView = inflater.inflate(R.layout.view_pulse_marker, parent, false);
+                viewHolder.foregroundImageView = (ImageView) convertView.findViewById(R.id.foreground_imageView);
+                viewHolder.backgroundImageView = (ImageView) convertView.findViewById(R.id.background_imageview);
+                convertView.setTag(viewHolder);
+            }
+            return convertView;
+        }
+
+        private static class ViewHolder {
+            ImageView foregroundImageView;
+            ImageView backgroundImageView;
+        }
+    }
+
+    private interface GeocodeCallbacks {
+        void onResult(String result);
+        void onFailure(Throwable failure);
     }
 
 }
