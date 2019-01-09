9fcee700203a2c3190fd995ab8970df23d19caa8
Isaac Sanchez <jelito@gmail.com>
2019-01-09 01:02:30 +0000

    Suport Message List + general styling revamp

--- app/src/main/java/cit/jauc/Constants.java
+++ app/src/main/java/cit/jauc/Constants.java
@@ -3,6 +3,7 @@ package cit.jauc;
 class Constants {
     static final String BOOKINGSURL = "https://jauc-ae38e.firebaseio.com/bookings";
     static final String RATINGSURL = "https://jauc-ae38e.firebaseio.com/reviews";
+    static final String SUPPORTURL = "https://jauc-ae38e.firebaseio.com/support";
     static final String CARSURL = "https://jauc-ae38e.firebaseio.com/cars";
     static final String INVOICESURL = "https://jauc-ae38e.firebaseio.com/invoices";
     static final String USERURL = "https://jauc-ae38e.firebaseio.com/users";
---
+++ app/src/main/java/cit/jauc/model/SupportMessage.java
@@ -0,0 +1,92 @@
+package cit.jauc.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SupportMessage implements Serializable {
+
+    private String id;
+    private String body;
+    private Date date;
+    private String email;
+    private String photoUrl;
+    private boolean read;
+    private String userId;
+    private String userName;
+
+
+    public SupportMessage() {
+        this.setBody("");
+        this.setDate(new Date());
+        this.setEmail("");
+        this.setPhotoUrl("");
+        this.setRead(false);
+        this.setUserId("");
+        this.setUserName("");
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
