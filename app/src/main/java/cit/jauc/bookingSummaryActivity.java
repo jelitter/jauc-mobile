3c20d8220eaa656b41a68a59b370dc473a543432
JesusPartal <duncan.rsr@gmail.com>
2019-01-14 12:58:40 +0000

    Maps upgrade (#39)
    * Update booking summary with trip route
    
    * Minor updates on bookingSummaryActivity
    
    * Remove unused imports

--- app/src/main/java/cit/jauc/bookingSummaryActivity.java
+++ app/src/main/java/cit/jauc/bookingSummaryActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,13 +25,32 @@ import cit.jauc.lib.HttpHandler;
 import cit.jauc.model.Booking;
 import cit.jauc.model.Location;
 
-public class bookingSummaryActivity extends AppCompatActivity {
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+
+
+
+public class bookingSummaryActivity extends AppCompatActivity implements OnMapReadyCallback {
     private Intent receiveIntent;
 
-    private TextView tvDestinationAddress;
     private String destinationAddress;
     private String originAddress;
-    private TextView tvOriginAddress;
 
     private Button btnCancel;
     private Button btnConfirm;
@@ -50,22 +68,30 @@ public class bookingSummaryActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private String currentUser;
 
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private DirectionsRoute currentRoute;
+    private NavigationMapRoute navigationMapRoute;
+
+
     private static final String TAG = "bookingSummaryActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_summary);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser().getUid();
 
         btnCancel = findViewById(R.id.bt_cancel);
         btnConfirm = findViewById(R.id.bt_confirm);
 
-
-        tvDestinationAddress = findViewById(R.id.tv_destination);
-        tvOriginAddress = findViewById(R.id.tv_origin);
-
+        // Get the putExtra data from the destinationActivity
         receiveIntent = getIntent();
         destinationAddress = receiveIntent.getStringExtra("destinationAddress");
         destinationLat = receiveIntent.getDoubleExtra("destinationLat",0);
@@ -73,9 +99,6 @@ public class bookingSummaryActivity extends AppCompatActivity {
         originAddress = receiveIntent.getStringExtra("originAddress");
         originLat = receiveIntent.getDoubleExtra("originLat", 0);
         originLon = receiveIntent.getDoubleExtra("originLon", 0);
-
-        tvDestinationAddress.setText(destinationAddress);
-        tvOriginAddress.setText(originAddress);
 
         originLocation = new Location();
         originLocation.setLat(originLat);
@@ -106,8 +129,72 @@ public class bookingSummaryActivity extends AppCompatActivity {
                 new BookingToDatabase().execute();
             }
         });
+    }
 
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
 
+        // Add the origin marker
+        mapboxMap.addMarker(new MarkerOptions()
+                .position(new LatLng(originLat, originLon))
+                .title("Origin")
+                .snippet(originAddress)
+        );
+
+        // Add the destination marker
+        mapboxMap.addMarker(new MarkerOptions()
+                .position(new LatLng(destinationLat, destinationLon))
+                .title("Destination")
+                .snippet(destinationAddress)
+        );
+        getRoute(Point.fromLngLat(originLon, originLat), Point.fromLngLat(destinationLon, destinationLat));
+
+        LatLng originLatLng = new LatLng(originLat, originLon);
+        LatLng destinationLatLang = new LatLng(destinationLat, destinationLon);
+
+        // Set the bounds of the camera with the origin and destination point
+        LatLngBounds latLngBounds = new LatLngBounds.Builder()
+                .include(originLatLng)
+                .include(destinationLatLang)
+                .build();
+        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, 50));
+
+    }
+
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(this)
+                .accessToken(getString(R.string.mapbox_access_token))
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        Log.d(TAG, "Response code: " + response.code());
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Log.e(TAG, "No routes found");
+                            return;
+                        }
+                        currentRoute = response.body().routes().get(0);
+
+                        // Draw the route on the map
+                        if (navigationMapRoute != null) {
+                            navigationMapRoute.removeRoute();
+                        } else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView, mapboxMap, R.style.NavigationMapRoute);
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                        Log.e(TAG, "Error: " + t.getMessage());
+                    }
+                });
     }
 
     private class BookingToDatabase extends AsyncTask<String, Integer, String> {
@@ -172,5 +259,47 @@ public class bookingSummaryActivity extends AppCompatActivity {
     public void onBackPressed() {
         Intent backIntent = new Intent(getBaseContext(), destinationActivity.class);
         startActivity(backIntent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
